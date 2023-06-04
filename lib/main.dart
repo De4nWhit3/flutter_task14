aa8cd0d132d0bc29a8375815d9cde26285f3279d
Dean White <de4nwhit3@gmail.com>
2023-06-04 03:18:34 +0200

    added questions

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:flutter/material.dart';
 
+import 'question.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -28,10 +30,92 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final List<Question> questions = [];
+
   @override
   void initState() {
     super.initState();
-    // initialize list
+    questions.add(
+      Question(
+        question: 'What type of Digimon is Agumon?',
+        answer: 'Vaccine',
+        category: 'digimon',
+        imageUrl: 'assets/agumon.jpg',
+        hints: [
+          'It\'s a term also used for immunity against diseases.',
+          'It\'s considered the strongest type in the show.'
+        ],
+      ),
+    );
+    questions.add(
+      Question(
+        question: 'Can Gabumon remove its fur?',
+        answer: 'Yes',
+        category: 'digimon',
+        imageUrl: 'assets/gabumon.jpg',
+        hints: [
+          'In the show he refers to his fur as a coat.',
+          'In the series when Matt is freezing he gives him the fur.'
+        ],
+      ),
+    );
+    questions.add(
+      Question(
+        question: 'What animal does Gomamon summon as his strongest spell?',
+        answer: 'Fish',
+        category: 'digimon',
+        imageUrl: 'assets/gomamon.jpg',
+        hints: [
+          'In England it goes well with fries.',
+          'It\'s rich in Omega oils.'
+        ],
+      ),
+    );
+    questions.add(
+      Question(
+        question: 'What is the name of Palmon\'s evolution?',
+        answer: 'Lillymon',
+        category: 'digimon',
+        imageUrl: 'assets/palmon.jpg',
+        hints: [
+          'It\'s the name of a flower.',
+          'It\'s popular on rivers and still dams.'
+        ],
+      ),
+    );
+    questions.add(
+      Question(
+        question: 'What was Patamon\'s evolution inspired by?',
+        answer: 'Angel',
+        category: 'digimon',
+        imageUrl: 'assets/patamon.jpg',
+        hints: [
+          'It\'s popular in the Bible.',
+          'Finish the sentence: Your guardian ???'
+        ],
+      ),
+    );
+    questions.add(
+      Question(
+        question: 'What is Piymon obsessed with?',
+        answer: 'Himself',
+        category: 'digimon',
+        imageUrl: 'assets/piyomon.jpg',
+        hints: [
+          'Some people are also obsessed by it.',
+          'If you constantly admire yourself in the mirror with who are you obsessed?'
+        ],
+      ),
+    );
+    questions.add(
+      Question(
+        question: 'What is the name of Tentomon\'s owner?',
+        answer: 'Izzy',
+        category: 'digimon',
+        imageUrl: 'assets/tentomon.jpg',
+        hints: ['He\'s a short boy.', 'He was the IT nerd in the series.'],
+      ),
+    );
   }
 
   @override
@@ -39,19 +123,69 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       body: AnimationLimiter(
         child: ListView.builder(
-          itemCount: 10,
+          itemCount: questions.length,
           itemBuilder: (BuildContext context, int index) {
             return AnimationConfiguration.staggeredList(
               position: index,
               duration: const Duration(seconds: 1),
-              child: const SlideAnimation(
+              child: SlideAnimation(
                 verticalOffset: 50.0,
                 child: FadeInAnimation(
-                  child: Text('Hello potatoes'),
+                  child: Column(
+                    children: [
+                      QuestionCard(
+                        question: questions[index],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                    ],
+                  ),
                 ),
               ),
             );
           },
+        ),
+      ),
+    );
+  }
+}
+
+class QuestionCard extends StatelessWidget {
+  final Question question;
+  const QuestionCard({super.key, required this.question});
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle textStyle = const TextStyle(color: Colors.white);
+    return Container(
+      color: Colors.red.shade900,
+      child: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                SizedBox(
+                    height: 60,
+                    width: 60,
+                    child: Image(
+                      fit: BoxFit.cover,
+                      image: AssetImage(question.imageUrl),
+                    )),
+                const SizedBox(
+                  width: 10,
+                ),
+                Flexible(
+                  child: Text(
+                    'Question: ${question.question}',
+                    style: textStyle,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
--- lib/question.dart
+++ lib/question.dart
@@ -2,13 +2,15 @@ class Question {
   final String question;
   final String answer;
   final String category;
+  final String imageUrl;
   final List<String> hints;
 
   Question({
     required this.answer,
-    required this.hints,
     required this.question,
     required this.category,
+    required this.imageUrl,
+    required this.hints,
   });
 
   (String, String, String, List<String>) getInfo() {
